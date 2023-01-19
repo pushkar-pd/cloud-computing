6c24a59ffb777846bf19b22d98e5aa81ee3851fd
pushkar-pd <pushkardhande11@gmail.com>
2023-01-19 08:21:40 +0530

    merged the feature of arithmetic operations

--- calculator.py
+++ calculator.py
@@ -1,15 +1,27 @@
 def add(num1,num2):
     return num1+num2
 
+def sub(num1,num2):
+    return num1-num2
+
+def mul(num1,num2):
+    return num1*num2
+
+def div(num1,num2):
+    return num1/num2
+
 res_add=add(10,20)
 print(res_add)
+
+res_sub=sub(10,20)
+print(res_sub)
+
+res_mul=mul(10,20)
+print(res_mul)
+
+res_div=div(10,20)
+print(res_div)
 
 
 #token of github
 #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
-
-depcfevciughl
-
-
-esgru
-huj
